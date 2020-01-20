b206fa0eaffca3419ff1007b582528a2cc1322dd
hun-a <seunghun.chan@gmail.com>
2020-01-21 00:21:23 +0900

    Add my first sprite sheet

---
+++ js/big-head.js
@@ -0,0 +1,24 @@
+class BigHead extends Phaser.Scene {
+  constructor() {
+    super('BigHead');
+  }
+
+  preload() {
+    this.load.spritesheet(
+      'man', 'assets/spritesheets/big_head.png',
+      { frameWidth: 32, frameHeight: 32 }
+    );
+  }
+
+  create() {
+    this.anims.create({
+      key: 'man_anim',
+      frames: this.anims.generateFrameNumbers('man'),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.man = this.add.sprite(config.width / 2, config.height / 2, 'man');
+    this.man.play('man_anim');
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+var config = {
+  width: 32,
+  height: 32,
+  backgroundColor: 0xFFFFFF,
+  scene: [BigHead]
+};
+
+var game = new Phaser.Game(config);
