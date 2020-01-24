1158cd9aa2c2a0569543c0f7c0590b69b019f372
hun-a <seunghun.chan@gmail.com>
2020-01-24 22:25:46 +0900

    Add the physics to the man and potions

--- js/big-head.js
+++ js/big-head.js
@@ -6,11 +6,20 @@ class BigHead extends Phaser.Scene {
       y: config.height / 2
     };
     this.speed = 1;
+    this.life = 3;
   }
 
   preload() {
     this.load.spritesheet(
       'man', 'assets/spritesheets/big_head.png',
+      { frameWidth: 32, frameHeight: 32 }
+    );
+    this.load.spritesheet(
+      'blue_potion', 'assets/spritesheets/blue_potion.png',
+      { frameWidth: 32, frameHeight: 32 }
+    );
+    this.load.spritesheet(
+      'red_potion', 'assets/spritesheets/red_potion.png',
       { frameWidth: 32, frameHeight: 32 }
     );
   }
@@ -20,6 +29,18 @@ class BigHead extends Phaser.Scene {
     this.position.y = pointer.y;
   }
 
+  increaseMoving(man, potion) {
+    this.speed = Math.min(this.speed + 1, 5);
+    potion.disableBody(true, true);
+    this.bluePotions.children.entries.pop();
+  }
+
+  heal(man, potion) {
+    this.life = Math.min(this.life + 1, 3);
+    potion.disableBody(true, true);
+    this.redPotions.children.entries.pop();
+  }
+
   create() {
     this.anims.create({
       key: 'man_anim',
@@ -27,10 +48,28 @@ class BigHead extends Phaser.Scene {
       frameRate: 10,
       repeat: -1
     });
+    this.anims.create({
+      key: 'blue_potion_anim',
+      frames: this.anims.generateFrameNumbers('blue_potion'),
+      frameRate: 10,
+      repeat: -1
+    });
+    this.anims.create({
+      key: 'red_potion_anim',
+      frames: this.anims.generateFrameNumbers('red_potion'),
+      frameRate: 10,
+      repeat: -1
+    });
 
-    this.man = this.add.sprite(this.position.x, this.position.y, 'man');
+    this.man = this.physics.add.sprite(this.position.x, this.position.y, 'man');
     this.man.play('man_anim');
     this.input.on('pointerdown', this.setPosition, this);
+
+    this.bluePotions = this.physics.add.group();
+    this.redPotions = this.physics.add.group();
+
+    this.physics.add.overlap(this.man, this.bluePotions, this.increaseMoving, null, this);
+    this.physics.add.overlap(this.man, this.redPotions, this.heal, null, this);
   }
 
   getMovingWeight() {
@@ -39,12 +78,37 @@ class BigHead extends Phaser.Scene {
   }
 
   moveTheMan() {
+    console.log(this.speed)
     var weight = this.getMovingWeight();
     this.man.x += (this.speed * weight.x);
     this.man.y += (this.speed * weight.y);
   }
 
+  isCreateTime() {
+    return Math.random() * 10000 < 10;
+  }
+
+  createBluePotion() {
+    if (!this.bluePotions.children.size && this.isCreateTime()) {
+      var bluePotion = this.physics.add.sprite(32, 32, 'blue_potion');
+      this.bluePotions.add(bluePotion);
+      bluePotion.setRandomPosition(0, 0, config.width - 16, config.height - 16);
+      bluePotion.play('blue_potion_anim');
+    }
+  }
+
+  createRedPotion() {
+    if (!this.redPotions.children.size && this.isCreateTime()) {
+      var redPotion = this.physics.add.sprite(32, 32, 'red_potion');
+      this.redPotions.add(redPotion);
+      redPotion.setRandomPosition(0, 0, config.width - 16, config.height - 16);
+      redPotion.play('red_potion_anim')
+    }
+  }
+
   update() {
     this.moveTheMan();
+    this.createBluePotion();
+    this.createRedPotion();
   }
 }
--- js/main.js
+++ js/main.js
@@ -2,7 +2,13 @@ var config = {
   width: 480,
   height: 640,
   backgroundColor: 0xFFFFFF,
-  scene: [BigHead]
+  scene: [BigHead],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  }
 };
 
 var game = new Phaser.Game(config);
